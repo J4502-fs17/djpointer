6338c5a274475e270953b4d2b0af0672548d06c4
ayethatsdj <djpointer@mwc-119163.dhcp.missouri.edu>
2017-11-10 14:14:55 -0600

    cndjn jdnkc ens

--- menu.js
+++ menu.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function(){
-$('menu-trigger').click(function(){
- $('#nav').slideToggle(500);
+$('.menu-trigger').click(function(){
+ $('.nav').slideToggle(500);
  });
  $(window).resize(function(){
    if ( $(window).width()>500){
-     $('#nav').removeAttr('style');
+     $('.nav').removeAttr('style');
    }
  });
 });
